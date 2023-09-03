3f3858f22c140dc4f6003eaa4025b5b0374ea4e2
Erik Pöhler <erik.poehler@fork.de>
2023-09-03 10:25:51 +0200

    update dependencies, fix redirect

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -47,8 +47,8 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('chuckle'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('app_chuckle_index'));
+        throw new \Exception('TODO: provide a valid redirect inside ' . __FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
